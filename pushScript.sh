7b0b9db594fa6d45dc09ca0e67d83478d6c65852
MrGarrett45 <gjmclaughlin19@gwmail.gwu.edu>
2017-07-07 12:21:30 -0400

    updated pushscript for graphs

--- pushScript.sh
+++ pushScript.sh
@@ -3,6 +3,8 @@ git add pushScript.sh
 git add RESULTS.txt
 git add trumpData.db
 git add hourFile.db
+git add dailyGraphs/
+git add totalGraphs/
 
 git commit -m "Daily update"
 git push origin master
